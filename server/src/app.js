36dfd6e347b15974e44e40b89a13088a0428b377
Patrick Oryono <patricken08@gmail.com>
2017-12-09 19:51:32 +0300

    initial commit

---
+++ server/src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Hello World'
+  })
+})
+app.listen(process.env.PORT || 8081)
